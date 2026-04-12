6902c17236bf7f7660a113a96ca8e66a70063a92
Nes <nesmain786@gmail.com>
2026-04-12 23:17:35 +0530

    Main Menu fix

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,11 +1,44 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     public string levelSelectScene = "LevelSelect";
 
+    [Header("UI")]
+    public Button newGameButton;
+    public TMP_Text newGameText;
+
+    void Start()
+    {
+        if (HasSave())
+        {
+            newGameText.text = "Continue";
+            newGameButton.onClick.RemoveAllListeners();
+            newGameButton.onClick.AddListener(LoadGame);
+        }
+        else
+        {
+            newGameText.text = "New Game";
+            newGameButton.onClick.RemoveAllListeners();
+            newGameButton.onClick.AddListener(StartNewGame);
+        }
+    }
+
+    bool HasSave()
+    {
+        return PlayerPrefs.HasKey("HasSave");
+    }
+
     public void StartNewGame()
+    {
+        PlayerPrefs.SetInt("HasSave", 1); // mark save exists
+        SceneManager.LoadScene(levelSelectScene);
+    }
+
+    public void LoadGame()
     {
         SceneManager.LoadScene(levelSelectScene);
     }
